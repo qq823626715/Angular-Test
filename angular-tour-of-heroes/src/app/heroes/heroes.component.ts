3ffbad6bb2d25c6a18891ce64c081b2ac7ecde35
LiuShengRong <liu823626715@icloud.com>
2018-10-25 10:59:51 +0800

    (+*) 添加mock-heroes文件内容，并引入到heroes中，显示在页面上

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',//组件选择器
   templateUrl: './heroes.component.html',//页面模板位置
@@ -8,10 +10,7 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id : 1,
-    name : 'Windstorm'
-  };
+  heroes = HEROES;
 
   constructor() { }
 
