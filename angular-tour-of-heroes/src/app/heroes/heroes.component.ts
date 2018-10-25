b6b914b0034ba364cc36d771115b4959b85bab24
LiuShengRong <liu823626715@icloud.com>
2018-10-25 10:13:13 +0800

    (*) 修改hero组件

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'app-heroes',//组件选择器
+  templateUrl: './heroes.component.html',//页面模板位置
+  styleUrls: ['./heroes.component.css']//私有样式位置
 })
 export class HeroesComponent implements OnInit {
+
+  hero = 'Windstorm';
 
   constructor() { }
 
   ngOnInit() {
+    //创建完组件之后调用该方法
+    //可用于初始化逻辑
   }
 
 }
