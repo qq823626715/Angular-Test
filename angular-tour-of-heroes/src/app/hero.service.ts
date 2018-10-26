2791f79f12aeb1b8a3f1795c0fcd9f1cabe81e69
LiuShengRong <liu823626715@icloud.com>
2018-10-26 15:45:45 +0800

    (*) 增加hero的delect方法

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -67,7 +67,7 @@ export class HeroService {
   updateHero (hero: Hero): Observable<any>{
     const httpOptions = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    }
+    };
     return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
       tap(_ => this.log(`updated hero id =${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
@@ -77,10 +77,23 @@ export class HeroService {
   addHero (hero: Hero): Observable<Hero> {
     const httpOptions = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    }
+    };
     return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
       tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
     )
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero (hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
   }
 }
