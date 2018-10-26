94f4de04748bd99d0a361ba2917a0c3a5a8e036e
LiuShengRong <liu823626715@icloud.com>
2018-10-26 15:40:39 +0800

    (*) 增加添加hero的方法

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -10,13 +10,14 @@ import { MessageService} from "./message.service";
 @Injectable({
   providedIn: 'root'
 })
+
 export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
 
   constructor(
     private http: HttpClient,
-    private messageService: MessageService
+    private messageService: MessageService,
   ) { }
 
 
@@ -66,10 +67,20 @@ export class HeroService {
   updateHero (hero: Hero): Observable<any>{
     const httpOptions = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    };
+    }
     return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
       tap(_ => this.log(`updated hero id =${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
+    )
+  }
+
+  addHero (hero: Hero): Observable<Hero> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    }
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
     )
   }
 }
